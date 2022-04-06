0b56a5fd89772743300b7554ab17bf3bc512f36f
Kleber Rodrigues <kleb3r.jr@gmail.com>
2022-04-06 14:03:53 -0300

    polindo a estrutura do banco

--- banco-de-dados.sql
+++ banco-de-dados.sql
@@ -1,18 +1,18 @@
 # Este script gera a estrutura do banco de dados.
 
-DROP TABLE IF EXISTS listaDePedidosRes;
-DROP TABLE IF EXISTS listaDePedidosEnt;
-DROP TABLE IF EXISTS produtospedidos;
+DROP TABLE IF EXISTS pedidoRestaurante;
+DROP TABLE IF EXISTS pedidosEntregador;
+DROP TABLE IF EXISTS produtosPedidos;
 DROP TABLE IF EXISTS pedido;
 DROP TABLE IF EXISTS enderecoCliente;
 DROP TABLE IF EXISTS enderecoRestaurante;
 DROP TABLE IF EXISTS cliente;
 DROP TABLE IF EXISTS entregador;
-DROP TABLE IF EXISTS gerente;
-DROP TABLE IF EXISTS login;
 DROP TABLE IF EXISTS menu;
 DROP TABLE IF EXISTS produto;
 DROP TABLE IF EXISTS restaurante;
+DROP TABLE IF EXISTS gerente;
+DROP TABLE IF EXISTS login;
 
 CREATE TABLE login (
 	codigo INT AUTO_INCREMENT,
@@ -23,38 +23,29 @@ CREATE TABLE login (
     PRIMARY KEY (codigo)
 );
 
-CREATE TABLE restaurante (
-	cnpj VARCHAR(18),
-    nome VARCHAR(45),
-    aberto BOOLEAN,
-    horarioAbertura TIME,
-    horarioFechamento TIME,
-    taxaDeEntrega DOUBLE,
-    
-    PRIMARY KEY (cnpj)
-);
-
 CREATE TABLE gerente (
 	registro INT AUTO_INCREMENT,
     cpf VARCHAR(20),
     nome VARCHAR(45),
-    restauranteCnpj VARCHAR(18),
     loginCodigo INT,
     
     PRIMARY KEY (registro, cpf),
-    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(Cnpj),
     FOREIGN KEY (loginCodigo) REFERENCES login(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE cliente (
 	cpf VARCHAR(20),
     nome VARCHAR(45),
     dataNascimento DATE,
-    telefone VARCHAR(11),
+    telefone VARCHAR(15),
     loginCodigo INT,
     
     PRIMARY KEY (cpf),
     FOREIGN KEY (loginCodigo) REFERENCES login(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE entregador (
@@ -66,6 +57,23 @@ CREATE TABLE entregador (
     
     PRIMARY KEY (registro),
     FOREIGN KEY (loginCodigo) REFERENCES login(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE restaurante (
+	cnpj VARCHAR(18),
+    nome VARCHAR(45),
+    aberto BOOLEAN,
+    horarioAbertura TIME,
+    horarioFechamento TIME,
+    taxaDeEntrega DOUBLE,
+    gerenteRegistro INT,
+    
+    PRIMARY KEY (cnpj),
+    FOREIGN KEY (gerenteRegistro) REFERENCES gerente(registro)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE produto (
@@ -78,6 +86,8 @@ CREATE TABLE produto (
     
     PRIMARY KEY (codigo),
     FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE enderecoRestaurante (
@@ -91,6 +101,8 @@ CREATE TABLE enderecoRestaurante (
     
     PRIMARY KEY (cep),
     FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(Cnpj)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE enderecoCliente (
@@ -104,6 +116,8 @@ CREATE TABLE enderecoCliente (
     
     PRIMARY KEY (cep),
     FOREIGN KEY (clienteCpf) REFERENCES cliente(cpf)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE menu (
@@ -112,8 +126,12 @@ CREATE TABLE menu (
     quantidade INT,
     
     PRIMARY KEY (restauranteCnpj, produtoCodigo),
-    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj),
+    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (produtoCodigo) REFERENCES produto(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE pedido (
@@ -127,28 +145,44 @@ CREATE TABLE pedido (
     enderecoCliente VARCHAR(11),
     
 	PRIMARY KEY (codigo),
-    FOREIGN KEY (clienteCpf) REFERENCES cliente(cpf),
-    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj),
-    FOREIGN KEY (entregadorRegistro) REFERENCES entregador(registro),
+    FOREIGN KEY (clienteCpf) REFERENCES cliente(cpf)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (entregadorRegistro) REFERENCES entregador(registro)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (enderecoCliente) REFERENCES enderecoCliente(cep)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-CREATE TABLE listaDePedidoRes (
+CREATE TABLE pedidoRestaurante (
 	restauranteCnpj VARCHAR(18),
     pedidoCodigo INT,
     
     PRIMARY KEY (restauranteCnpj),
-    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj),
+    FOREIGN KEY (restauranteCnpj) REFERENCES restaurante(cnpj)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (pedidoCodigo) REFERENCES pedido(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-CREATE TABLE listaDePedidosEnt (
+CREATE TABLE pedidosEntregador (
 	entregadorRegistro INT,
     pedidoCodigo INT,
     
     PRIMARY KEY (entregadorRegistro),
-    FOREIGN KEY (entregadorRegistro) REFERENCES entregador(registro),
+    FOREIGN KEY (entregadorRegistro) REFERENCES entregador(registro)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (pedidoCodigo) REFERENCES pedido(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE produtosPedidos (
@@ -158,8 +192,12 @@ CREATE TABLE produtosPedidos (
     pedidoCodigo INT,
     
     PRIMARY KEY (pedidoCodigo),
-    FOREIGN KEY (produtoCodigo) REFERENCES produto(codigo),
+    FOREIGN KEY (produtoCodigo) REFERENCES produto(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (pedidoCodigo) REFERENCES pedido(codigo)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
  
 
