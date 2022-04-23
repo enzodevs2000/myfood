626b9edff2c459eb8446a599245dc7c0b787a979
Kleber Rodrigues <kleb3r.jr@gmail.com>
2022-04-23 16:15:08 -0300

    adicionando controllers e services

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
 
+server.use('/api', routes);
+
 server.listen(process.env.PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
 })
