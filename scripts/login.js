d0bedf2f25a07ebf040cd744112d44d1f38f055e
Kleber Rodrigues <kleb3r.jr@gmail.com>
2022-04-26 20:47:47 -0300

    finalizando validação e recuperação de dados na página de login, além de arrumar estilização

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,84 @@
+/* FETCH FUNCTIONS ================================================== */
+
+/** Faz a requisição à API procurando por um login específico.
+ * 
+ * @param {String} email O email do login a ser procurado no banco de dados.
+ * @return {Array} Um array com os dados do login, caso exista.
+ */
+async function searchLogin(email) {
+    const initialFetch = await fetch(`http://localhost:3000/api/login/${email}`);
+
+    const jsonData = await initialFetch.json();
+
+    return jsonData.result;
+}
+
+
+/* AUXILIARY FUNCTIONS ============================================== */
+
+/** Valida os dados inseridos pelo usuário na tela de login.
+ * 
+ * @return {Object} Um objeto com informações sobre o login. A primeira
+ * posição do objeto diz se o login é válido, enquanto a segunda contém 
+ * um array com os dados do login recuperados do banco.
+ */
+async function validateLogin() {
+    const email = document.querySelector('#email').value;
+    const senha = document.querySelector('#password').value;
+    
+    const result = await searchLogin(email);
+    
+    if (result.length != 0 && result.senha == senha) {
+        return {valid: true, data: result};
+    }
+        
+    return {valid: false, data: result};
+}
+
+
+/* EVENT HANDLERS =================================================== */
+
+/** Persiste os dados do login no sessionStorage do navegador 
+ * e redireciona o usuário para a página de acordo, caso seu login
+ * exista no banco de dados.
+ */
+async function submitForm() {
+    const login = await validateLogin();
+    
+    if (login.valid) {
+        sessionStorage.setItem('user', login.data);
+
+        switch (login.data.permissao) {
+            case 1:
+                window.location.replace("./perfil-cliente.html");
+                break;
+            case 2:
+                window.location.replace("./perfil-gerente.html");
+                break;
+            default:
+                window.location.replace("./perfil-entregador.html");
+                break;
+        }
+        
+    } else {
+        alert('Login não é válido!');
+    }
+}
+
+
+/* EVENT LISTENERS ================================================== */
+
+const form = document.querySelector('form');
+form.addEventListener('submit', submitForm);
+
+
 // GET ----------------------------------------------------------- //
     
-fetch('http://localhost:3000/api/cliente')
-    .then(response => response.json())
-    .then(jsonObj => {
-        console.log(jsonObj.result);
-    })
+// fetch('http://localhost:3000/api/login/empresa1@example.com')
+//     .then(response => response.json())
+//     .then(jsonObj => {
+//         console.log(jsonObj.result);
+//     })
 
 
 
